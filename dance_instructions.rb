c32b70d8a4e17bb5f5cc701a4824275c678633ca
Jayden Sansom <jaydensansom@gmail.com>
2018-07-23 20:04:52 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,9 @@
 
 # Code your methods below
+def lowerbody
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+  
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip step"
+  
